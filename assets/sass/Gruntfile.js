b288208b4f3595cc778a92ff467b2e3b6a8b3ccf
fatooo0116@gmail.com <cbsi940424>
2022-09-26 12:18:53 +0800

    update

---
+++ assets/sass/Gruntfile.js
@@ -0,0 +1,39 @@
+
+
+
+
+
+module.exports = function(grunt) {
+
+
+// require('load-grunt-tasks')(grunt);
+
+
+ 
+grunt.initConfig({
+    sass: {
+        dist: {
+            files: {
+                '../css/style.css': 'sass/style.scss'
+            }
+        }
+    },
+    watch: {
+        scripts: {
+          files: ['sass/style.scss','sass/*.scss','sass/tpl/*.scss'],
+          tasks: ['sass'],
+          options: {
+          //  event: ['added', 'deleted'],
+          },
+        },
+      },
+});
+
+
+grunt.loadNpmTasks('grunt-contrib-watch');
+grunt.loadNpmTasks('grunt-contrib-sass');
+
+
+grunt.registerTask('default', ['sass','watch']);
+
+};
